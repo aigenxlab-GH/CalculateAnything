0a1fe34a004eb306163d24de84030ed9fe5fbcfb
aigenxlab-GH <aigenxlab@gmail.com>
2026-05-25 19:18:32 +0530

    updated SEO

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",   // generate static files in /out
-  trailingSlash: true, // Netlify serves /page/ correctly
+  output: "export",    // generate static files in /out
+  trailingSlash: true, // ensures /page/ URLs map to /page/index.html
 };
 
 export default nextConfig;
