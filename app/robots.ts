b2a0e4e45c4a5e694a47ccd86e75a117ba1ce2af
aigenxlab-GH <aigenxlab@gmail.com>
2026-06-22 13:01:12 +0530

    fix: block /_next/static/ from crawler in robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/_next/static/',
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
